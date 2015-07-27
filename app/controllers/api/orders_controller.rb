3a681af5099b56664c96397bf2aede2c1f26e99e
Kamil Górski <kamil@freakone.pl>
2015-07-27 10:23:03 +0200

    item model fix, json nested serialization

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -10,7 +10,7 @@ def users
 end
 
 def show
-  render json: Order.find(params[:id]), :include => [:items, :user => {:only => [:name]}]
+  render json: Order.find(params[:id]), :include => {:items => { :include => { :user => {only: :name}}}}
 end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :order
+  belongs_to :user
   validates :order_id, :name, :price, presence: true
 end
