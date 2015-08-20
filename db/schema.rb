be5e7864dec36cefa86f133bfdc6a01c4ffb9e66
Kamil Górski <kamil@freakone.pl>
2015-08-20 13:16:06 +0200

    bigint uid fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710190724) do
+ActiveRecord::Schema.define(version: 20150820110932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20150710190724) do
     t.string   "name"
     t.string   "oauth_token"
     t.string   "oauth_expires"
-    t.integer  "uid"
+    t.integer  "uid",           limit: 8
     t.string   "image"
   end
 
